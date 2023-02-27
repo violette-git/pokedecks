8186bb7fd6a4feca2fae2b5cab99137e92943411
violette-git <ideasforyang@gmail.com>
2023-02-26 20:54:41 -0600

    deplo

--- pokedex_org_proj/settings.py
+++ pokedex_org_proj/settings.py
@@ -159,3 +159,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DJANGO_SETTINGS_MODULE= 'pokedex.settings'
